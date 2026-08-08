b6fb8811e94c2d01b7a189c7a664ba0b02dc465d
Sandip-Maurya <77878319+Sandip-Maurya@users.noreply.github.com>
2026-08-08 16:24:33 +0530

    Add: sql-sales-person - submission-0

---
+++ SQL/sql-sales-person/submission-0.sql
@@ -0,0 +1,13 @@
+-- Write your query below
+-- Write your query below
+
+
+select s.name 
+from sales_person s
+where not exists (
+    select 1 
+    from orders o
+    join company c
+        on c.com_id = o.com_id
+    where o.sales_id = s.sales_id and c.name = 'CRIMSON'
+);
