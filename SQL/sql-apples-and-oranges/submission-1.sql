1ad0acc2b3e50f97df996ccaae5823a37f37025e
Sandip-Maurya <77878319+Sandip-Maurya@users.noreply.github.com>
2026-08-16 15:42:34 +0530

    Add: sql-apples-and-oranges - submission-1

---
+++ SQL/sql-apples-and-oranges/submission-1.sql
@@ -0,0 +1,12 @@
+-- Write your query below
+
+select 
+    sale_date,
+    sum(
+        case when fruit = 'apples' then sold_num else - sold_num
+        end 
+    ) as diff 
+
+from sales 
+group by sale_date
+order by sale_date;
